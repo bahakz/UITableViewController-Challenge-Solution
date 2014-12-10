3f7929e0535ae9e392f202170fa7831aa8fbc663
bahakz <bahakz@gmail.com>
2014-12-10 14:29:36 +0600

    project is done

--- UITableViewController Challenge Solution/CustomUITableViewController.h
+++ UITableViewController Challenge Solution/CustomUITableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CustomUITableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Bakytzhan Baizhikenov on 12/10/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CustomUITableViewController : UITableViewController
 
 @end
-
