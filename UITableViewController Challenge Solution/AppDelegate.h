0026c4c5bb2b84f4dfdb47b2c0f6e513113e76b6
bahakz <bahakz@gmail.com>
2014-12-10 14:07:15 +0600

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Bakytzhan Baizhikenov on 12/10/14.
+//  Copyright (c) 2014 intellection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Bakytzhan Baizhikenov on 12/10/14.
+//  Copyright (c) 2014 intellection. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
